e1ac3882666a0645be15855eb6c722587c158e72
Ranno Rajaste <raraja@ttu.ee>
2020-05-07 21:48:49 +0300

    Update linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,27 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+      node: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
+      'plugin:vue/essential',
+      '@vue/standard',
+      '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+      ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'indent': ['error', 4],
+      'quotes': 'off',
+      'semi': 'off',
+      'space-before-function-paren': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/interface-name-prefix': [
+          "error",
+          { "prefixWithI": "always"}
+      ]
   }
 }
